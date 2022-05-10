4835206217bcf67174469a96c1d7729fd0297b0e
S M Zahurul Islam <zahurul.it@gmail.com>
2022-05-10 14:54:12 +0600

    Add files via upload
    project complete

---
+++ app.js
@@ -0,0 +1,19 @@
+let product = [{
+        Name: 'Laptop',
+        Company: "HP",
+        Price: 80000,
+        Quantity: 7
+    }, {
+        Name: "Laptop",
+        Company: "Apple",
+        Price: 250000,
+        Quantity: 7
+    },
+    {
+        Name: "Laptop",
+        Company: "Microsoft",
+        Price: 180000,
+        Quantity: 6
+    }
+]
+console.table(product);
